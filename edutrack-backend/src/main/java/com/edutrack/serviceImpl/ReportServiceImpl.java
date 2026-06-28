cc46e9f226ea3ada373b90040523ef5017dfbd3b
Ugeshraja <ugeshraja007@gmail.com>
2026-06-28 11:10:52 +0530

    Fix student database

--- edutrack-backend/src/main/java/com/edutrack/serviceImpl/ReportServiceImpl.java
+++ edutrack-backend/src/main/java/com/edutrack/serviceImpl/ReportServiceImpl.java
@@ -12,11 +12,13 @@ import com.edutrack.repository.StudentRepository;
 import com.edutrack.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class ReportServiceImpl implements ReportService {
 
     @Autowired
--- edutrack-backend/src/main/java/com/edutrack/serviceImpl/StudentServiceImpl.java
+++ edutrack-backend/src/main/java/com/edutrack/serviceImpl/StudentServiceImpl.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class StudentServiceImpl implements StudentService {
 
     @Autowired
